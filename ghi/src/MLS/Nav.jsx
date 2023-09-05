f9a74196b1ae6c986bc3ed0efb87b5675dabe63a
Austin Sekel <giants38914@gmail.com>
2023-09-05 19:26:12 -0400

    almost finished with list, detail & card pages

--- ghi/src/MLS/Nav.jsx
+++ ghi/src/MLS/Nav.jsx
@@ -33,9 +33,6 @@ const Nav = () => {
                         {account && <li className="nav-item">
                             <NavLink to={'/login'} className={'nav-link'}>Login</NavLink>
                         </li>}
-                        {account && <li className="nav-item">
-                            <NavLink to={'/logout'} className={'nav-link'}>Logout</NavLink>
-                        </li>}
                     </ul>
                     {account && <button className="btn btn-outline-danger" onClick={logout}>
                         Logout
@@ -45,47 +42,3 @@ const Nav = () => {
         </nav>
     )
 };
-
-export default Nav;
-
-import React from 'react';
-import { NavLink, useNavigate } from "react-router-dom";
-import { useAccountQuery, useLogout } from "../app/apiSlice";
-
-export default function Nav() {
-    const { data: account } = useAccountQuery();
-    const navigate = useNavigate();
-    const [logout] = useLogout();
-    return (
-        <nav className="navbar navbar-expand-lg overlay">
-            <div className="container-fluid flex-nowrap">
-            <NavLink className={"navbar-brand"} to="mls/home">
-                HOME
-            </NavLink>
-            <NavLink className={"navbar-brand"} to="mls/create-account">
-                CREATE ACCOUNT
-            </NavLink>
-            <NavLink className={"navbar-brand"} to="mls/team-detail">
-                TEAM DETAIL
-            </NavLink>
-            <NavLink className={"navbar-brand"} to="mls/login">
-                LOGIN
-            </NavLink>
-            <NavLink className={"navbar-brand"} to="mls/logout">
-                LOGOUT
-            </NavLink>
-            {account && (
-                <button
-                className="btn btn-outline-danger logout"
-                onClick={() => {
-                    logout();
-                    navigate("/");
-                }}
-                >
-                LOGOUT
-                </button>
-            )}
-            </div>
-        </nav>
-    );
-}
