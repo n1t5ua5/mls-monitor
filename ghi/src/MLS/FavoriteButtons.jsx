e702d5ea8eeecd9cfdf0cd7b85642d37f48ca701
Ethan <ethanwilson89@gmail.com>
2023-09-11 10:51:30 -0600

    Journals completed

--- ghi/src/MLS/FavoriteButtons.jsx
+++ ghi/src/MLS/FavoriteButtons.jsx
@@ -1,42 +1,45 @@
 import {
-    useGetFavoritesForAccountQuery,
-    useDeleteFavoriteMutation,
-    useCreateFavoriteMutation
+  useGetFavoritesForAccountQuery,
+  useDeleteFavoriteMutation,
+  useCreateFavoriteMutation,
 } from "./app/apiSlice";
 import { useState, useEffect } from "react";
 
-const FavoriteButtons = ({name}) => {
-    const [deleteFavorite] = useDeleteFavoriteMutation();
-    const [createFavorite, createFavoriteStatus] = useCreateFavoriteMutation();
-    const [favorite, setFavorite] = useState();
-    const { data: favorites } = useGetFavoritesForAccountQuery();
+const FavoriteButtons = ({ name }) => {
+  const [deleteFavorite] = useDeleteFavoriteMutation();
+  const [createFavorite, createFavoriteStatus] = useCreateFavoriteMutation();
+  const [favorite, setFavorite] = useState();
+  const { data: favorites } = useGetFavoritesForAccountQuery();
 
-    console.log({createFavoriteStatus})
+  console.log({ createFavoriteStatus });
 
-    useEffect(() => {
-        if (favorites) {
-            const match = favorites.find(f => f.pokemon_name === name)
-            setFavorite(match);
-        }
-    }, [favorites])
+  useEffect(() => {
+    if (favorites) {
+      const match = favorites.find((f) => f.name === name);
+      setFavorite(match);
+    }
+  }, [favorites]);
 
-    return (
-        <>
-            {!favorite && <button
-                className="btn btn-success"
-                onClick={() => createFavorite({pokemon_name: name})}
-            >
-                Favorite
-            </button>}
-            {favorite && <button
-                className="btn btn-danger"
-                onClick={() => deleteFavorite(favorite)}
-            >
-                Unfavorite
-            </button>
-                }
-        </>
-    )
-}
+  return (
+    <>
+      {!favorite && (
+        <button
+          className="btn btn-success"
+          onClick={() => createFavorite({ name: name })}
+        >
+          Favorite
+        </button>
+      )}
+      {favorite && (
+        <button
+          className="btn btn-danger"
+          onClick={() => deleteFavorite(favorite)}
+        >
+          Unfavorite
+        </button>
+      )}
+    </>
+  );
+};
 
 export default FavoriteButtons;
