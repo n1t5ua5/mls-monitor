deaad52d220e905538bbf6851bb9edd30661ffc3
Austin Sekel <giants38914@gmail.com>
2024-01-25 12:42:37 -0500

    reverted change

--- ghi/src/MLS/Login.jsx
+++ ghi/src/MLS/Login.jsx
@@ -21,44 +21,42 @@ const LoginForm = () => {
   };
 
   return (
-      <div className="wrapper">
-        <h2>Login</h2>
-        <form onSubmit={handleSubmit}>
-          <div className="input-box">
-            <input
-              placeholder="Email"
-              required
-              type="text"
-              name="username"
-              id="username"
-              className="form-control"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              autoComplete="username"
-            />
-            <label htmlFor="username"></label>
-          </div>
+    <div className="wrapper">
+      <h2>Login</h2>
+      <form onSubmit={handleSubmit}>
+        <div className="input-box">
+          <input
+            placeholder="Email"
+            required
+            type="text"
+            name="username"
+            id="username"
+            className="form-control"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <label htmlFor="username"></label>
+        </div>
 
-          <div className="input-box">
-            <input
-              placeholder="Password"
-              required
-              type="password"
-              name="password"
-              id="password"
-              className="form-control"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              autoComplete="current-password"
-            />
-            <label htmlFor="password"></label>
-          </div>
-          <button className="button" type="submit">
-            Submit
-          </button>
-        </form>
-        <Footer />
-      </div>
+        <div className="input-box">
+          <input
+            placeholder="Password"
+            required
+            type="password"
+            name="Password"
+            id="password"
+            className="form-control"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <label htmlFor="password"></label>
+        </div>
+        <button className="button" type="submit">
+          Submit
+        </button>
+      </form>
+      <Footer />
+    </div>
   );
 };
 
